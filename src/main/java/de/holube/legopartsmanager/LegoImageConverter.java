2e68ab454f0021a8fa85074f654d41d4800548fe
Tilman Holube <tilman.holube@t-online.de>
2022-02-22 11:43:35 +0100

    finish image converter

--- src/main/java/de/holube/legopartsmanager/LegoImageConverter.java
+++ src/main/java/de/holube/legopartsmanager/LegoImageConverter.java
@@ -21,33 +21,33 @@ public class LegoImageConverter {
         Set<Integer> allowedColorIDs = new HashSet<>();
         allowedColorIDs.add(0); //Black
         allowedColorIDs.add(15); //White
-        /*allowedColorIDs.add(7); //Light Gray
-        allowedColorIDs.add(8); //Dark Gray
+        //allowedColorIDs.add(7); //Light Gray
+        //allowedColorIDs.add(8); //Dark Gray
         allowedColorIDs.add(71); //Light Bluish Gray
         allowedColorIDs.add(72); //Dark Bluish Gray
         allowedColorIDs.add(1); //Blue
-        allowedColorIDs.add(9); //Light Blue
-        allowedColorIDs.add(2); //Green
-        allowedColorIDs.add(17); //Light Green
-        allowedColorIDs.add(10); //Bright Green
-        allowedColorIDs.add(27); //Lime
-        allowedColorIDs.add(4); //Red
-        allowedColorIDs.add(320); //Dark Red
-        allowedColorIDs.add(25); //Orange
-        allowedColorIDs.add(14); //Yellow
+        //allowedColorIDs.add(9); //Light Blue
+        //allowedColorIDs.add(2); //Green
+        //allowedColorIDs.add(17); //Light Green
+        //allowedColorIDs.add(10); //Bright Green
+        //allowedColorIDs.add(27); //Lime
+        //allowedColorIDs.add(4); //Red
+        //allowedColorIDs.add(320); //Dark Red
+        //allowedColorIDs.add(25); //Orange
+        //allowedColorIDs.add(14); //Yellow
         allowedColorIDs.add(13); //Pink
-        allowedColorIDs.add(29); //Bright Pink
-        allowedColorIDs.add(5); //Dark Pink*/
+        //allowedColorIDs.add(29); //Bright Pink
+        //allowedColorIDs.add(5); //Dark Pink
         Log.debug("Loading Image");
-        BufferedImage image = loadImage("mietz.jpg");
-        int left = 120;
-        int right = 80;
-        int top = 100;
-        int bottom = 100;
+        BufferedImage image = loadImage("mietz2.jpg");
+        int left = 324;
+        int right = image.getWidth() - left - 2716;
+        int top = 392;
+        int bottom = image.getHeight() - top - 3708;
         image = image.getSubimage(left, top, image.getWidth() - left - right, image.getHeight() - top - bottom);
 
         Log.debug("Scaling Image");
-        image = scale(image, 3, 4);
+        image = scale(image, 2, 3);
 
         Log.debug("Replace and count Pixels");
         Map<Integer, Integer> colorFrequency = new HashMap<>();
@@ -62,7 +62,12 @@ public class LegoImageConverter {
         }
 
         Log.debug("Save Image");
-        saveImage(image, "testExport.png");
+        saveImage(scaleUp(image, 50), "testExport.png");
+        for (int x = 0; x < image.getWidth(); x = x + 16) {
+            for (int y = 0; y < image.getHeight(); y = y + 16) {
+                saveImage(scaleUp(image.getSubimage(x, y, 16, 16), 50), "export" + x + "_" + y + ".png");
+            }
+        }
 
         colorFrequency.forEach((colorID, value) -> Log.info(legoColorManager.getColor(colorID).getName() + ": " + value));
     }
@@ -79,7 +84,12 @@ public class LegoImageConverter {
         g.dispose();
 
         if (result.getHeight() < height) {
-            //return result.getSubimage(0, (result.getHeight() % 16) / 2, width, result.getHeight() - (result.getHeight() % 16));
+            int addedHeight = 16 - (result.getHeight() % 16);
+            BufferedImage tmp = new BufferedImage(result.getWidth(), result.getHeight() + addedHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics g2 = tmp.createGraphics();
+            g2.drawImage(result, 0, addedHeight / 2, result.getWidth(), result.getHeight(), null);
+            g2.dispose();
+            return tmp;
         } else if (result.getHeight() > height) {
             return result.getSubimage(0, 0, width, height);
         }
@@ -103,5 +113,13 @@ public class LegoImageConverter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static BufferedImage scaleUp(BufferedImage image, int factor) {
+        BufferedImage tmp = new BufferedImage(image.getWidth() * factor, image.getHeight() * factor, BufferedImage.TYPE_INT_ARGB);
+        Graphics g2 = tmp.createGraphics();
+        g2.drawImage(image, 0, 0, image.getWidth() * factor, image.getHeight() * factor, null);
+        g2.dispose();
+        return tmp;
     }
 }
